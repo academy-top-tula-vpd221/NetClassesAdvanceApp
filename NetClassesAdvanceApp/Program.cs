1d142881018ea54c3a6d72856aab6acb9dc9c088
Ефимов Максим <efimov_m@itstep.academy>
2023-03-19 14:10:41 +0300

    19-03-23

--- NetClassesAdvanceApp/Program.cs
+++ NetClassesAdvanceApp/Program.cs
@@ -1,8 +1,18 @@
 ﻿using NetUserClassLib;
+using System.Runtime.CompilerServices;
 
 User user = new User();
 
 user.Age = 10;
+
+//UserInfo userInfo = new UserInfo();
+
+//UserInfo.Count = 10;
+
+var builder = House.GetBuilder(5);
+
+House house1 = new(5);
+House house2 = new(15);
 
 /*
 
@@ -27,3 +37,70 @@ class Example
     public float Sum(float a, float b) { return a + b; }
     public int Sum(int a, int b, int c) { return a + b + c; }
 }
+
+class House
+{
+    const int N = 10;
+    public readonly int M;
+    static House()
+    {
+
+    }
+    public House(int m)
+    {
+        M = m;
+    }
+    public bool Wall { set; get; }
+    public bool Garage { set; get; }
+    public bool Garden { set; get; }
+    public bool Pool { set; get; }
+    public static HouseBuilder GetBuilder(int m)
+    {
+        return new HouseBuilder(new House(m));
+    }
+
+    public HouseBuilder GetBuilder(bool f = true)
+    {
+        return new HouseBuilder(this);
+    }
+}
+
+class HouseBuilder
+{
+    House house;
+    public HouseBuilder(House house)
+    {
+        this.house = house;
+    }
+    HouseBuilder SetWall()
+    {
+        house.Wall = true;
+        return this;
+    }
+
+    HouseBuilder SetGarage()
+    {
+        house.Garage = true;
+        return this;
+    }
+
+    HouseBuilder SetGarden()
+    {
+        house.Garden = true;
+        return this;
+    }
+
+    HouseBuilder SetPool()
+    {
+        house.Pool = true;
+        return this;
+    }
+
+    House Build()
+    {
+        return house;
+    }
+}
+
+readonly struct Person { readonly string Name; readonly int Age; }
+record struct PersonR(string Name, int Age);
